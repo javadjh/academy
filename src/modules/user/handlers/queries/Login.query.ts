9c568da5c23ca0d9862816f147f150a1215c34b8
javad <48103478+javadjh@users.noreply.github.com>
2025-10-13 12:12:04 +0330

    fix

--- src/modules/user/handlers/queries/Login.query.ts
+++ src/modules/user/handlers/queries/Login.query.ts
@@ -7,6 +7,7 @@ import { Password } from 'src/utility/password';
 import { RecordNotFoundException } from 'src/filters/record-not-found.filter';
 import { Auth } from 'src/config/Auth';
 import { Response } from 'src/config/response';
+import { Semester, SemesterDocument } from 'src/schema/semester.schema';
 
 export class LoginQuery {
   constructor(public readonly dto: LoginRequestDto) {}
@@ -16,6 +17,8 @@ export class LoginQuery {
 export class LoginHandler implements IQueryHandler<LoginQuery> {
   constructor(
     @InjectModel(User.name) private readonly userModel: Model<UserDocument>,
+    @InjectModel(Semester.name)
+    private readonly semesterModel: Model<SemesterDocument>,
 
     private readonly auth: Auth,
   ) {}
@@ -29,9 +32,13 @@ export class LoginHandler implements IQueryHandler<LoginQuery> {
     if (!(await Password.compare(password, user.password)))
       throw new RecordNotFoundException();
 
+    const semester = await this.semesterModel.findOne().sort({ createdAt: -1 });
+
     let token: string = await this.auth.generateToken({
       sub: user?._id,
       userType: user?.userType,
+      semester: semester?._id,
+      department: user.department,
     });
 
     return Response.send(token);
